30fba344d88269ef8c0b5877a68bb1131d831716
gzsodhy <ehxinf@126.com>
2022-07-11 21:13:46 +0800

    config eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,55 +1,37 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a
-      href="https://code.visualstudio.com/"
-      target="_blank"
-    >VS Code</a>
-    +
-    <a
-      href="https://github.com/johnsoncodehk/volar"
-      target="_blank"
-    >Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a
-      href="https://vitejs.dev/guide/features.html"
-      target="_blank"
-    >
-      Vite Docs
-    </a>
-    |
-    <a
-      href="https://v3.vuejs.org/"
-      target="_blank"
-    >Vue 3 Docs</a>
-  </p>
-
+  <h1>Hello WOrld</h1>
+  <p>{{ props.msg }}</p>
+  <p>{{ count }}</p>
   <button
     type="button"
-    @click="count++"
+    @click="increment"
   >
-    count is: {{ count }}
+    Increment
   </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+<script setup lang="ts">
+import { ref, defineProps, defineEmits } from 'vue'
 
+const props = defineProps({
+  msg: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['increment'])
+
+const count = ref(100)
+const increment = () => {
+  count.value++
+  emit('increment')
+}
+
+</script>
 <style scoped>
 a {
   color: #42b983;
